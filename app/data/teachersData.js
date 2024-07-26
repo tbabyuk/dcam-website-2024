73f54fcf22bdb2fe80a23075422e07dabf8e0636
Terry Babyuk <tbabyuk@gmail.com>
2024-07-25 23:12:10 -0400

    removed Chloe bio, altered Terry bio

--- app/data/teachersData.js
+++ app/data/teachersData.js
@@ -29,15 +29,15 @@ export const teachersArray = [
       instrument: ["drums"],
       bio: "Originally from Havana, Cuba, Raul started his musical career at the age of eight. Dazzled by the sound of drums and percussion, he took lessons with several private teachers, who encouraged him to pursue music as a profession. After finishing his studies in Cuba, he immigrated to Canada in 2005 and continued his studies at Humber College, where he finished his Degree in “Contemporary Music” in 2010. During his time at Humber, he was awarded the Millennium Scholarship and, as a member of the Humber Studio Jazz Ensemble, gained valuable experience working with world class artists such as Mike Stern, Randy Brecker, Dave Douglas, Labarbera Brothers, Chris Potter and others. Today, he teaches and continues performing and recording professionally in the city of Toronto."  
     },
-    {
-      name: "Chloe",
-      fullName: "Chloe Dionne",
-      photo: "/images/teachers/chloe.jpg",
-      alt: "voice teacher Chloe",
-      featured: true,
-      instrument: ["vocals", "beginner piano"],
-      bio: "Chloé is currently a vocal performance master's student at the University of Toronto. She is passionate about singing in many genres such as opera, pop, jazz, and musical theater. Chloe has performed around Nova Scotia in many events, independently, and at the Fountain School of Performing Arts where she received her Bachelors of Music degree. With the Fountain School, she was in several operas, was a prize winner in their 2023 Concerto Night, and a performer in their 2022 reopening gala performing in the same concert as artists such as Jeremy Dutcher and Kiefer Sutherland. She is also a songwriter and composer. She was recently selected to participate in a music production workshop with Nova Scotian artist and producer Erin Costelo. As a teacher, she strives to give students the confidence to expand their musical world while giving them a solid technical foundation."
-    },
+    // {
+    //   name: "Chloe",
+    //   fullName: "Chloe Dionne",
+    //   photo: "/images/teachers/chloe.jpg",
+    //   alt: "voice teacher Chloe",
+    //   featured: true,
+    //   instrument: ["vocals", "beginner piano"],
+    //   bio: "Chloé is currently a vocal performance master's student at the University of Toronto. She is passionate about singing in many genres such as opera, pop, jazz, and musical theater. Chloe has performed around Nova Scotia in many events, independently, and at the Fountain School of Performing Arts where she received her Bachelors of Music degree. With the Fountain School, she was in several operas, was a prize winner in their 2023 Concerto Night, and a performer in their 2022 reopening gala performing in the same concert as artists such as Jeremy Dutcher and Kiefer Sutherland. She is also a songwriter and composer. She was recently selected to participate in a music production workshop with Nova Scotian artist and producer Erin Costelo. As a teacher, she strives to give students the confidence to expand their musical world while giving them a solid technical foundation."
+    // },
     {
       name: "Giancarlo",
       fullName: "Giancarlo Gianneo",
@@ -110,6 +110,6 @@ export const teachersArray = [
       alt: "piano teacher Terry",
       featured: false,
       instrument: ["piano", "music theory"],
-      bio: "Taras (Terry) Babyuk started his musical journey with accordion lessons at the age of seven. In his teens, he began piano lessons and soon decided to dedicate himself fully to this instrument. Terry holds Level 9 RCM Certificate in piano and has taught beginner to intermediate piano and beginner to advanced music theory for many years (a self-described music theory nerd!). In addition to his fondness for Classical and Romantic-era music, he also loves film music and enjoys composing original piano music in his spare time. Aside from his teaching duties at DCAM, he is also its co-founder and Managing Director."
+      bio: "Taras (Terry) Babyuk started his musical journey with accordion lessons at the age of seven. In his teens, he became fascinated with piano and never looked back. In addition to his 10+ years of experience in the field of education, Terry holds Level 9 RCM Certificate in piano and has taught beginner to high intermediate piano and beginner to advanced music theory for many years. In addition to his fondness for Classical and Romantic-Era music, he also loves film music and enjoys composing original piano pieces in his spare time. Aside from his occasional teaching duties at DCAM, he is also its co-founder and Managing Director."
     },
 ]
